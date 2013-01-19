926ab6e22af514005b19c5e9c06a3e191c6b779a
laverya <laverya@umich.edu>
2013-01-19 12:35:02 -0500

    Updated code, removed enormous block of commented out useless stuff

--- AndroidApp/SmoothRide/src/com/ummaps/smoothride/MainActivity.java
+++ AndroidApp/SmoothRide/src/com/ummaps/smoothride/MainActivity.java
@@ -12,59 +12,6 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-/*
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        LocationManager locManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-//        LocationListener locListener = new LocationListener();
-//        LocationProvider provider = locManager.getProvider(LocationManager.GPS_PROVIDER);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
-    LocationListener locListener = new LocationListener() {
-    	TextView latlongtv = (TextView) findViewById(R.id.textView1);
-    	
-    	@Override
-    	public void onLocationChanged(Location loc){
-    		double latitude = loc.getLatitude();
-    		double longitude = loc.getLongitude();
-    		String tvtext = "Lat = " + latitude + "\nLong = " + longitude;
-    		latlongtv.setText(tvtext);
-    	}
-
-		@Override
-		public void onProviderDisabled(String provider) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void onProviderEnabled(String provider) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void onStatusChanged(String provider, int status, Bundle extras) {
-			// TODO Auto-generated method stub
-			
-		}
-    }; 
-    
- //   locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListner);)
-    
- }
-*/
 
 
 public class MainActivity extends Activity {
@@ -79,30 +26,29 @@ public class MainActivity extends Activity {
         locManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, locListener);
     }
 
-   /* To test in the emulator, use the telnet commands:
-    * telnet localhost 5554
-    * geo fix 30.0 30.0 */
-
-    /* Class My Location Listener */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
 
     public class MyLocationListener implements LocationListener
     {
-        TextView tv = (TextView) findViewById(R.id.textView1);
+        TextView gpstv = (TextView) findViewById(R.id.textView1);
 
         @Override
         public void onLocationChanged(Location loc){
-            Log.d("tag", "Finding Latitude");
+        //    Log.d("tag", "Finding Latitude");
             double lat = loc.getLatitude();
-            Log.d("tag", "Lat: "+String.valueOf(lat));
-            Log.d("tag", "Finding Longitude");
+        //    Log.d("tag", "Lat: "+String.valueOf(lat));
+        //    Log.d("tag", "Finding Longitude");
             double lon = loc.getLongitude();
-            Log.d("tag", "Lon: "+String.valueOf(lon));
-            String Text = "My current location is: " +
-            "\nLatitude = " + lat +
-            "\nLongitude = " + lon;
+        //    Log.d("tag", "Lon: "+String.valueOf(lon));
+            String outText = "Latitude = " + lat + "\nLongitude = " + lon;
 
             // Display location
-            tv.setText(Text);
+            gpstv.setText(outText);
         }
 
         @Override
