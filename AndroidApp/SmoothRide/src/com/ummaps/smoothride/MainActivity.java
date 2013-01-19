af1d73326bb4f7619327f39495ad71723ebbfcd2
laverya <laverya@umich.edu>
2013-01-19 12:19:43 -0500

    LOCATION WORKS! Updates every time location changes, pushes to screen.

--- AndroidApp/SmoothRide/src/com/ummaps/smoothride/MainActivity.java
+++ AndroidApp/SmoothRide/src/com/ummaps/smoothride/MainActivity.java
@@ -12,7 +12,7 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
+/*
 public class MainActivity extends Activity {
 
     @Override
@@ -64,4 +64,60 @@ public class MainActivity extends Activity {
  //   locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListner);)
     
  }
+*/
 
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /* Use the LocationManager class to obtain GPS locations */
+        LocationManager locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locListener = new MyLocationListener();
+        locManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, locListener);
+    }
+
+   /* To test in the emulator, use the telnet commands:
+    * telnet localhost 5554
+    * geo fix 30.0 30.0 */
+
+    /* Class My Location Listener */
+
+    public class MyLocationListener implements LocationListener
+    {
+        TextView tv = (TextView) findViewById(R.id.textView1);
+
+        @Override
+        public void onLocationChanged(Location loc){
+            Log.d("tag", "Finding Latitude");
+            double lat = loc.getLatitude();
+            Log.d("tag", "Lat: "+String.valueOf(lat));
+            Log.d("tag", "Finding Longitude");
+            double lon = loc.getLongitude();
+            Log.d("tag", "Lon: "+String.valueOf(lon));
+            String Text = "My current location is: " +
+            "\nLatitude = " + lat +
+            "\nLongitude = " + lon;
+
+            // Display location
+            tv.setText(Text);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider){
+            Toast.makeText(getApplicationContext(), "Gps Disabled", Toast.LENGTH_SHORT ).show();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider){
+            Toast.makeText(getApplicationContext(), "Gps Enabled", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras){
+
+        }
+    }
+}
