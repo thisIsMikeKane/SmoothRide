4561fae4faac9b517179a325029f0277489443a0
laverya <laverya@umich.edu>
2013-01-19 00:41:15 -0500

    updating text

--- AndroidApp/SmoothRide/gen/com/ummaps/smoothride/R.java
+++ AndroidApp/SmoothRide/gen/com/ummaps/smoothride/R.java
@@ -14,7 +14,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int edit_message=0x7f070000;
+        public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -24,8 +25,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int button_send=0x7f040002;
+        public static final int edit_message=0x7f040001;
+        public static final int hello_world=0x7f040003;
+        public static final int menu_settings=0x7f040004;
     }
     public static final class style {
         /** 
