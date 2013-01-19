375824ef9dbe2feee2302aaad16f163992db463b
laverya <laverya@umich.edu>
2013-01-19 12:50:17 -0500

    Added info page, still working on it Info should display on "about" screen

--- AndroidApp/SmoothRide/gen/com/ummaps/smoothride/R.java
+++ AndroidApp/SmoothRide/gen/com/ummaps/smoothride/R.java
@@ -19,17 +19,21 @@ public final class R {
         public static final int textView1=0x7f070001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_about=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_about=0x7f060000;
+        public static final int activity_main=0x7f060001;
     }
     public static final class string {
+        public static final int about=0x7f040006;
         public static final int app_name=0x7f040000;
         public static final int button_send=0x7f040002;
         public static final int edit_message=0x7f040001;
         public static final int hello_world=0x7f040003;
         public static final int menu_settings=0x7f040004;
+        public static final int title_activity_about=0x7f040005;
     }
     public static final class style {
         /** 
