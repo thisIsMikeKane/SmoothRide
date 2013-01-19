790f42784ced4b902336f0322bac08f67c271521
Brian Margosian <inventowiz@gmail.com>
2013-01-19 08:52:39 -0500

    Update accelerometer, pose question

--- Embedded/Arduino/accelerometer_cpp/accelerometer_cpp.ino
+++ Embedded/Arduino/accelerometer_cpp/accelerometer_cpp.ino
@@ -1,30 +1,61 @@
 #include <AcceleroMMA7361.h>
+#include <math.h>
+
+#define DEBUG
+
+void print(float x, float y, float z);
+void updateAccels();
 
 AcceleroMMA7361 accelero;
-int x;
-int y;
-int z;
+float x;
+float y;
+float z;
 
 void setup()
 {
   Serial.begin(9600);
-  accelero.begin(13, 12, 11, 10, A0, A1, A2);
-  accelero.setARefVoltage(3.3);                   //sets the AREF voltage to 3.3V
-  accelero.setSensitivity(LOW);                   //sets the sensitivity to +/-6G
-  accelero.calibrate();
+  accelero.begin(2, 5, 3, 4, A0, A1, A2);
+  accelero.setARefVoltage(3);
+  accelero.setSensitivity(LOW);
+  accelero.calibrate(); //make sure accel is flat before running
+  Serial.print("\n");
 }
 
 void loop()
 {
-  x = accelero.getXRaw();
-  y = accelero.getYRaw();
-  z = accelero.getZRaw();
-  Serial.print("\nx: ");
-  Serial.print(x);
-  Serial.print("\ty: ");
-  Serial.print(y);
-  Serial.print("\tz: ");
-  Serial.print(z);
-  delay(500);                                     //(make it readable)
+  //updateAccels();
+  
+  #ifdef DEBUG
+  print(x,y,z);
+  #endif
+  
+  double sum = 0;
+  for(int i=0;i<50;i++){
+    updateAccels();
+    sum += sqrt((x*x)+(y*y)+(z*z));
+  }
+  (sum /= 50)--;
+  
+  Serial.print(sum);
+  Serial.print("\n");
+  delay(1);
 }
 
+void print(float x, float y, float z){
+  Serial.print("x: ");
+  Serial.print(x);
+  Serial.print("g\ty: ");
+  Serial.print(y);
+  Serial.print("g\tz: ");
+  Serial.print(z);
+  Serial.print("g\n");
+}
+
+void updateAccels(){
+  x = accelero.getXAccel();
+  y = accelero.getYAccel();
+  z = accelero.getZAccel();
+  x /= 100;
+  y /= 100;
+  z /= 100;
+}
