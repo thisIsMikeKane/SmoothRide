ca2c01b9bf6abb605b6c189d38af09da48f5fa71
laverya <laverya@umich.edu>
2013-01-20 02:55:16 -0500

    Has post code, does not seem to be accepted yet.

--- AndroidApp/SmoothRide/src/com/ummaps/smoothride/MainActivity.java
+++ AndroidApp/SmoothRide/src/com/ummaps/smoothride/MainActivity.java
@@ -1,8 +1,21 @@
 package com.ummaps.smoothride;
 
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 import java.util.Set;
+import java.lang.Math;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
@@ -16,6 +29,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.os.StrictMode;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -41,6 +56,7 @@ public class MainActivity extends Activity implements Runnable, Handler.Callback
 	
 	// UI
 	private TextView			mTextViewStatus;
+	private TextView			tvTextViewSent;
 	private Spinner				mSpinnerDevices;
 	private ArrayAdapter		mArrayAdapterDevices;
 	private Handler				mHandler;
@@ -49,6 +65,10 @@ public class MainActivity extends Activity implements Runnable, Handler.Callback
 	private int					mCmdGetData;
 	private int					mSmallBump;
 	private int					mBigBump;
+	private int					mSmallBumpThresh;
+	private int					mBigBumpThresh;
+	private double 				distTrvld;
+	private double 				tempDistTrvld;
 	
 	public MainActivity() {
 		mIsThreadRunning			= false;
@@ -56,7 +76,9 @@ public class MainActivity extends Activity implements Runnable, Handler.Callback
 		mSPP						= new BlueSmirfSPP();
 		mArrayListBluetoothAddress	= new ArrayList<String>();
 		
-		mCmdGetData = 0xA7;
+		mCmdGetData = 0x64; // ASCI 'd'
+		mSmallBumpThresh = 25;
+		mBigBumpThresh = 5;
 	}
 	
 	/** Called when the activity is first created. */
@@ -68,6 +90,7 @@ public class MainActivity extends Activity implements Runnable, Handler.Callback
         // Initialize UI
         setContentView(R.layout.activity_main);
         mTextViewStatus			= (TextView) findViewById(R.id.ID_STATUS);
+        tvTextViewSent			= (TextView) findViewById(R.id.tvtextview);
         ArrayList<String> items = new ArrayList<String>();
         mSpinnerDevices			= (Spinner) findViewById(R.id.ID_PAIRED_DEVICES);
         mArrayAdapterDevices	= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
@@ -80,6 +103,11 @@ public class MainActivity extends Activity implements Runnable, Handler.Callback
         LocationManager locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
         LocationListener locListener = new MyLocationListener();
         locManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, locListener);
+        
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy); 
+        
+        
     }
 
     @Override
@@ -177,19 +205,83 @@ public class MainActivity extends Activity implements Runnable, Handler.Callback
     public class MyLocationListener implements LocationListener
     {
         TextView gpstv = (TextView) findViewById(R.id.textView1);
-
+        double latStart, lonStart;
+        boolean reachedChkPt = true;
+        
         @Override
         public void onLocationChanged(Location loc){
-        //    Log.d("tag", "Finding Latitude");
+
             double lat = loc.getLatitude();
-        //    Log.d("tag", "Lat: "+String.valueOf(lat));
-        //    Log.d("tag", "Finding Longitude");
             double lon = loc.getLongitude();
-        //    Log.d("tag", "Lon: "+String.valueOf(lon));
+            
+            
+            // After traveling distance, check to see if map should be updated
+            if (reachedChkPt) {
+            	latStart = lat;
+            	lonStart = lon;
+            	
+            	reachedChkPt = false;
+            	
+            	if (mBigBump > mBigBumpThresh) {
+            		mBigBump = 0;
+            		
+            		mHandler.sendEmptyMessage(0);
+            		postData(lat, lon, 1);
+            	}
+            	
+            	if (mSmallBump > mSmallBumpThresh) {
+            		mSmallBump = 0;
+            		
+            		mHandler.sendEmptyMessage(0);
+            		postData(lat, lon, 0);
+            	}
+            }
+            
+            tempDistTrvld = LatLong2km(lat, lon, latStart, lonStart);
+            if (tempDistTrvld > 0.005) {
+            	reachedChkPt = true;
+            	distTrvld = tempDistTrvld;
+            }
+            
             String outText = "Latitude = " + lat + "\nLongitude = " + lon;
 
             // Display location
             gpstv.setText(outText);
+        }
+        
+        public void postData(double lat, double lon, int howlarge) {
+            // Create a new HttpClient and Post Header
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://smoothride.crowdmap.com/api?report");
+            Calendar rightNow = Calendar.getInstance();
+
+            try {
+                // Add your data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                String blankstring = "";
+                nameValuePairs.add(new BasicNameValuePair("latitude", blankstring + lat));
+                nameValuePairs.add(new BasicNameValuePair("longitude", blankstring + lon));
+                nameValuePairs.add(new BasicNameValuePair("country_name", "United States"));
+                nameValuePairs.add(new BasicNameValuePair("incident_title","Pothole"));
+                nameValuePairs.add(new BasicNameValuePair("incident_description",blankstring + howlarge));
+                nameValuePairs.add(new BasicNameValuePair("incident_date",blankstring + Calendar.DATE + "/" + Calendar.MONTH + "/" + Calendar.YEAR));
+                nameValuePairs.add(new BasicNameValuePair("incident_hour",blankstring + Calendar.HOUR));
+                nameValuePairs.add(new BasicNameValuePair("incident_minute",blankstring + Calendar.MINUTE));
+                nameValuePairs.add(new BasicNameValuePair("incident_ampm",blankstring + Calendar.AM_PM));
+                nameValuePairs.add(new BasicNameValuePair("incident_category", blankstring + howlarge));
+                nameValuePairs.add(new BasicNameValuePair("location_name", "Pothole"));
+                nameValuePairs.add(new BasicNameValuePair("task", "report"));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                
+                tvTextViewSent.setText(blankstring + httppost);
+                
+                httppost.setHeader("Authorization", "Basic " + Base64.encodeToString("aozgaa@umich.edu:Aichwaigror=".getBytes(), Base64.NO_WRAP));
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+
+            } catch (ClientProtocolException e) {
+            } catch (IOException e) {
+            }
         }
 
         @Override
@@ -208,6 +300,22 @@ public class MainActivity extends Activity implements Runnable, Handler.Callback
         }
     }
     
+    // from http://www.movable-type.co.uk/scripts/latlong.html
+    private double LatLong2km(double lat1, double lon1, double lat2, double lon2) {
+    	double R = 6371.0f; // km
+    	double dLat = Math.toRadians(lat2-lat1);
+    	double dLon = Math.toRadians(lon2-lon1);
+    	lat1 = Math.toRadians(lat1);
+    	lat2 = Math.toRadians(lat2);
+    	
+    	double a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
+    			   Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+    	double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    	double d = R * c;
+    	
+    	return d;
+    }
+    
 	/*
 	 * main loop
 	 */  
@@ -221,19 +329,24 @@ public class MainActivity extends Activity implements Runnable, Handler.Callback
     		mSPP.flush();
     		
     		// Retrieve data
-    		mBigBump    = mSPP.readByte();
-    		mBigBump   |= mSPP.readByte() << 8;
-    		mSmallBump  = mSPP.readByte(); 
-    		mSmallBump |= mSPP.readByte() << 8;
+    		int tempBigBump    = mSPP.readByte();
+    		tempBigBump   |= mSPP.readByte() << 8;
+    		int tempSmallBump  = mSPP.readByte(); 
+    		tempSmallBump |= mSPP.readByte() << 8;
+    		
+    		mBigBump 	+= tempBigBump;
+    		mSmallBump	+= tempSmallBump;
     		
     		if (mSPP.isError()) {
     			mSPP.disconnect();
+    		} else {
+    			
     		}
     		
     		mHandler.sendEmptyMessage(0);
     		
     		// wait briefly before sending the next serial command
-    		try { Thread.sleep((long) (1000.0F/30.0F)); }
+    		try { Thread.sleep((long) (100000.0F/30.0F)); }
     		catch (InterruptedException e) { Log.e(TAG, e.getMessage());}
     		
     	}
@@ -243,6 +356,12 @@ public class MainActivity extends Activity implements Runnable, Handler.Callback
         mIsThreadRunning 	= false;
         mHandler.sendEmptyMessage(0);
     }   
+    
+    /*
+     * Get data from Arduino
+     */
+    
+    
     
 	/*
 	 * update UI
@@ -256,13 +375,16 @@ public class MainActivity extends Activity implements Runnable, Handler.Callback
     
     private void UpdateUI() {
     	if (mSPP.isConnected()) {
-    		mTextViewStatus.setText("connected to " + mSPP.getBluetoothAddress() + "\n" + 
-    								"Small bumps detected\t: " + mSmallBump + "\n" + 
-    								"Big bumps detected\t: " + mBigBump + "\n");
+    		mTextViewStatus.setText("Connected to " + mSPP.getBluetoothAddress() + "\n" + 
+    								"Small bumps detected: " + mSmallBump + "\n" + 
+    								"Big bumps detected: " + mBigBump + "\n" + 
+    								"Distance since last checkpoint: " + tempDistTrvld);
     	} else if (mIsThreadRunning) {
     		mTextViewStatus.setText("connecting to " + mBluetoothAddress);
     	} else {
     		mTextViewStatus.setText("disconnected");
     	}
+    	
+    	int aaa = 1;
     }
 }
